bb4ae1e92d218f2e9f21d9adff0371c478aec16f
sajjadmrf <sajjad.m2002@gmail.com>
2021-11-07 21:49:48 -0800

    some bug fixes

--- login.php
+++ login.php
@@ -1,16 +1,21 @@
+<?php
+$db = mysqli_connect('localhost','root','');
+mysqli_select-db($db,'db1');
+mysqli_query($db,"SET NAMES 'UTF8'");
+//fix bug
+$username = htmlentities(addslashes($_POST['username']),ENT_QOUTES,'UTF-8')
+$password = htmlentities(addslashes($_POST['password']),ENT_QOUTES,'UTF-8')
+$q = "SELECT * FROM users WHERE username = '$username'
+		AND password = '$password'";
+$r = mysqli_query($db,$q);
+$user = myaqli_fetch-assoc($r);
+if(isset($user['id']))
+{
+	print "Login OK";
+}
+else
+{
+	print "Login Failed";
+}
 
-qli_connect('localhost','root','');
-lect_db($db,'db1');
-ery($db,"SET NAMES 'UTF8'");
- = htmlentities(addslashes($_POST['username'];),ENT_QOUTES,'UTF-8');
- = htmlentities(addslashes($_POST['password'];),ENT_QOUTES,'UTF-8');
- ECT * FROM users WHERE username = '$username'
-    AND password = '$password'
-li-query($db,$q);
-ysqli_fetch-assoc($r);
-$user['id']))
-
- "Login OK";
-
-
- "Login Fail";
+?>
